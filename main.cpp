aad5bb1eda2703a3e31d1eb4b0a69f01631d0447
Luis Martinez <lemartinez5@csuchico.edu>
2024-09-18 23:30:11 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,31 +1,9 @@
 #include <iostream>
 
-using std::cout;
-using std::cin;
-using std::endl;
+     using namespace std ;
 
-int main(){
-  double first, second;
+     int main () {
+         cout << "Hello World!" << endl;
 
-  cout<< "First number: ";
-  cin>> first;
-  cout<< "Second number: ";
-  cin>> second;
-
-  cout<< "Addition: "<< first << "+" << second << "=";
-  cout<< (first+second) << endl;
-  cout<< "Subtraction: "<< first << "-" << second << "=";
-  cout<< (first-second) << endl;
-  cout<< "Multiplication: "<< first << "*" << second << "=";
-  cout<< (first*second) << endl;
-
-  if(second == 0){
-    cout << "Division: Error - cannot divide by zero" << endl;
-  } else {
-    cout<< "Division: "<< first << "/" << second << "=";
-    cout<< (first/second) << endl;
-  }
-
-
-  return 0;
-}
+         return 0;
+     }
